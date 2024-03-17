1a2cb27d39f561a659ffbc0737378e02264a6505
pi <pawel.i@frontkom.com>
2024-03-17 09:19:37 +0100

    add more content to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,27 @@ export default function Home() {
   return (
     <section className="p-8 flex flex-col h-full justify-center">
       <h2 className="text-3xl font-bold font-poppins">BIP</h2>
-      <div className="text-lg">Strona główna</div>
+
+      <p className="text-lg mt-5">
+        Jesteśmy Twoim niezawodnym źródłem najświeższych informacji dotyczących
+        działalności rządu. Nasza misja to zapewnienie transparentności i
+        dostępu do kluczowych decyzji oraz inicjatyw podejmowanych przez
+        administrację państwową.
+        <br />
+        <br />
+        Niezależnie od tego, czy interesuje Cię polityka społeczna, gospodarcza
+        czy kwestie środowiskowe, nasz biuletyn dostarczy Ci kompleksowych
+        analiz, raportów i najnowszych wiadomości. Znajdziesz tutaj także
+        informacje na temat planowanych zmian prawnych, programów rządowych oraz
+        sposobów uczestnictwa obywateli w życiu publicznym.
+        <br />
+        <br />
+        Nieustannie dążymy do doskonalenia naszej działalności, aby sprostać
+        oczekiwaniom społeczeństwa i zapewnić efektywną komunikację pomiędzy
+        rządem a obywatelami. Dziękujemy, że jesteś z nami, i zachęcamy do
+        regularnego odwiedzania Biuletynu Rządowego, aby być na bieżąco z
+        najważniejszymi wydarzeniami w życiu naszego kraju.
+      </p>
     </section>
   );
 }
